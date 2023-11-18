9cc3027a1f00ef8e90b5a6cb2dd109a3624a579f
Patrick Vroegh <patvroegh@gmail.com>
2023-11-18 20:16:40 +0100

    Adding rounding mode selector

--- src/WashTimer.App/Components/OffsetDisplay.razor.cs
+++ src/WashTimer.App/Components/OffsetDisplay.razor.cs
@@ -6,10 +6,40 @@ namespace WashTimer.App.Components
 {
     public partial class OffsetDisplay
     {
+        public enum RoundingMode
+        {
+            FifteenMinutes,
+            ThirtyMinutes,
+            SixtyMinutes
+        }
+
         [Inject]
         public IStringLocalizer<Resources> StringLocalizer { get; set; } = default!;
 
         [Parameter]
-        public TimeSpan Offset { get; set; }
+        public OffsetTimeResult Offset { get; set; } = OffsetTimeResult.Zero;
+
+        public TimeSpan RoundedOffset => Rounding switch
+        {
+            RoundingMode.FifteenMinutes => Offset.OffsetRoundedTo15Minutes,
+            RoundingMode.ThirtyMinutes => Offset.OffsetRoundedToHalfAnHour,
+            RoundingMode.SixtyMinutes => Offset.OffsetRoundedToOneHour
+        };
+
+        [Parameter]
+        public RoundingMode Rounding { get; set; } = RoundingMode.FifteenMinutes;
+
+        protected void OnFifteenMinutesRoundingClick()
+        {
+            Rounding = RoundingMode.FifteenMinutes;
+        }
+        protected void OnThirtyMinutesRoundingClick()
+        {
+            Rounding = RoundingMode.ThirtyMinutes;
+        }
+        protected void OnSixtyMinutesRoundingClick()
+        {
+            Rounding = RoundingMode.SixtyMinutes;
+        }
     }
 }
