317a3a9d12b3f96da6f98422142fa9d9144834d8
Patrick Vroegh <patvroegh@gmail.com>
2023-11-19 19:41:59 +0100

    Adding localization for en and nl

---
+++ src/WashTimer.App/HostExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace WashTimer.App
+{
+    public static class HostExtensions
+    {
+        public static async Task<WebAssemblyHost> AddCulture(this WebAssemblyHost host)
+        {
+            CultureInfo culture;
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+            if (result != null)
+            {
+                culture = new CultureInfo(result);
+            }
+            else
+            {
+                culture = new CultureInfo("en-US");
+                await js.InvokeVoidAsync("blazorCulture.set", "en-US");
+            }
+
+            Console.WriteLine(culture);
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            return host;
+        }
+    }
+}
